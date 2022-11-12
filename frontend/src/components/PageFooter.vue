3099b887c14e5a5937bf7b88ca8e602ebeebbdd5
lhj <piyomon0327@naver.com>
2022-11-13 05:31:32 +0900

    Vue 게시판 만들기(2 ~ 10)

---
+++ frontend/src/components/PageFooter.vue
@@ -0,0 +1,17 @@
+<!-- PageFooter.vue -->
+<template>
+  <hr/>
+  <footer>
+    여기는 footer 자리입니다.
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "PageFooter"
+}
+</script>
+
+<style scoped>
+
+</style>
